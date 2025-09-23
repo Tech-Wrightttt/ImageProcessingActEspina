8edf6b1d5f1701fe536adf34a6960a49e5c606cb
jai <ruhmerjairus.espina@cit.edu>
2025-09-23 23:00:23 +0800

    First test

--- ImageProcessingActEspina/Form1.Designer.cs
+++ ImageProcessingActEspina/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addSecondImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subtractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newImageToolStripMenuItem,
+            this.addSecondImageToolStripMenuItem,
+            this.subtractToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newImageToolStripMenuItem
+            // 
+            this.newImageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.option1ToolStripMenuItem,
+            this.option2ToolStripMenuItem,
+            this.option3ToolStripMenuItem});
+            this.newImageToolStripMenuItem.Name = "newImageToolStripMenuItem";
+            this.newImageToolStripMenuItem.Size = new System.Drawing.Size(99, 24);
+            this.newImageToolStripMenuItem.Text = "New Image";
+            // 
+            // addSecondImageToolStripMenuItem
+            // 
+            this.addSecondImageToolStripMenuItem.Name = "addSecondImageToolStripMenuItem";
+            this.addSecondImageToolStripMenuItem.Size = new System.Drawing.Size(150, 24);
+            this.addSecondImageToolStripMenuItem.Text = "Add Second Image";
+            // 
+            // subtractToolStripMenuItem
+            // 
+            this.subtractToolStripMenuItem.Name = "subtractToolStripMenuItem";
+            this.subtractToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.subtractToolStripMenuItem.Text = "Subtract";
+            // 
+            // option1ToolStripMenuItem
+            // 
+            this.option1ToolStripMenuItem.Name = "option1ToolStripMenuItem";
+            this.option1ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.option1ToolStripMenuItem.Text = "Option 1";
+            // 
+            // option2ToolStripMenuItem
+            // 
+            this.option2ToolStripMenuItem.Name = "option2ToolStripMenuItem";
+            this.option2ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.option2ToolStripMenuItem.Text = "Option 2";
+            // 
+            // option3ToolStripMenuItem
+            // 
+            this.option3ToolStripMenuItem.Name = "option3ToolStripMenuItem";
+            this.option3ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.option3ToolStripMenuItem.Text = "Option 3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addSecondImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subtractToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem option1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem option2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem option3ToolStripMenuItem;
     }
 }
 
