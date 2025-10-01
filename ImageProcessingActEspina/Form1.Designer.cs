f0418e304a4f79b0347cb76ce74b5e1ac8fafd22
jai <ruhmerjairus.espina@cit.edu>
2025-10-01 16:24:52 +0800

    added first part again kay nawala

--- ImageProcessingActEspina/Form1.Designer.cs
+++ ImageProcessingActEspina/Form1.Designer.cs
@@ -28,94 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.newImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.addSecondImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.subtractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.option1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.option2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.option3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // openFileDialog1
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.newImageToolStripMenuItem,
-            this.addSecondImageToolStripMenuItem,
-            this.subtractToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(574, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "CHOOSE SECOND IMAGE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(396, 360);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "CHOOSE FIRST IMAGE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(151, 498);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "COLOR INVERSION";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 498);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 43);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "BASIC COPY";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 547);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 43);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "GREYSCALE";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(151, 547);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 43);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "HISTOGRAM";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(290, 498);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(133, 43);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "SEPIA";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // newImageToolStripMenuItem
             // 
             this.newImageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.option1ToolStripMenuItem,
-            this.option2ToolStripMenuItem,
-            this.option3ToolStripMenuItem});
+            this.option2ToolStripMenuItem});
             this.newImageToolStripMenuItem.Name = "newImageToolStripMenuItem";
-            this.newImageToolStripMenuItem.Size = new System.Drawing.Size(99, 24);
-            this.newImageToolStripMenuItem.Text = "New Image";
-            // 
-            // addSecondImageToolStripMenuItem
-            // 
-            this.addSecondImageToolStripMenuItem.Name = "addSecondImageToolStripMenuItem";
-            this.addSecondImageToolStripMenuItem.Size = new System.Drawing.Size(150, 24);
-            this.addSecondImageToolStripMenuItem.Text = "Add Second Image";
-            // 
-            // subtractToolStripMenuItem
-            // 
-            this.subtractToolStripMenuItem.Name = "subtractToolStripMenuItem";
-            this.subtractToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
-            this.subtractToolStripMenuItem.Text = "Subtract";
+            this.newImageToolStripMenuItem.Size = new System.Drawing.Size(128, 24);
+            this.newImageToolStripMenuItem.Text = "CHOOSE MODE";
+            this.newImageToolStripMenuItem.Click += new System.EventHandler(this.newImageToolStripMenuItem_Click);
             // 
             // option1ToolStripMenuItem
             // 
             this.option1ToolStripMenuItem.Name = "option1ToolStripMenuItem";
-            this.option1ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.option1ToolStripMenuItem.Text = "Option 1";
+            this.option1ToolStripMenuItem.Size = new System.Drawing.Size(202, 26);
+            this.option1ToolStripMenuItem.Text = "NORMAL MODE";
             // 
             // option2ToolStripMenuItem
             // 
             this.option2ToolStripMenuItem.Name = "option2ToolStripMenuItem";
-            this.option2ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.option2ToolStripMenuItem.Text = "Option 2";
+            this.option2ToolStripMenuItem.Size = new System.Drawing.Size(202, 26);
+            this.option2ToolStripMenuItem.Text = "WEBCAM MODE";
             // 
-            // option3ToolStripMenuItem
+            // menuStrip1
             // 
-            this.option3ToolStripMenuItem.Name = "option3ToolStripMenuItem";
-            this.option3ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.option3ToolStripMenuItem.Text = "Option 3";
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newImageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1289, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 469);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "CHOOSE OPTIONS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox2.Location = new System.Drawing.Point(438, 31);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(396, 360);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox3.Location = new System.Drawing.Point(864, 31);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(396, 360);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1289, 1033);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
         private System.Windows.Forms.ToolStripMenuItem newImageToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem addSecondImageToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem subtractToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem option1ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem option2ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem option3ToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- ImageProcessingActEspina/Form1.cs
+++ ImageProcessingActEspina/Form1.cs
@@ -12,9 +12,310 @@ namespace ImageProcessingActEspina
 {
     public partial class Form1 : Form
     {
+
+        private Bitmap image1, image2, image3;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null && image1 != null)
+            {
+                image2 = new Bitmap(image1.Width, image1.Height);
+                for (int y = 0; y < image1.Height; y++)
+                {
+                    for (int x = 0; x < image1.Width; x++)
+                    {
+                        Color pixelColor = image1.GetPixel(x, y);
+                        int InversionValue1 = 255 - pixelColor.R;
+                        int InversionValue2 = 255 - pixelColor.G;
+                        int InversionValue3 = 255 - pixelColor.B;
+     
+                        Color newColor = Color.FromArgb(InversionValue1, InversionValue2, InversionValue3);
+                        //the most monkey ass inefficient code ever part 2
+                        image2.SetPixel(x, y, newColor);
+                    }
+                }
+
+
+                pictureBox2.Image = image2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+            else
+            {
+                MessageBox.Show("Please load an image first!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null && image1 != null)
+            {
+                image2 = new Bitmap(image1.Width, image1.Height);
+                for (int y = 0; y < image1.Height; y++) { 
+                    for (int x = 0; x < image1.Width; x++) {
+                        Color pixelColor = image1.GetPixel(x, y);
+                        image2.SetPixel(x, y, pixelColor);
+                    }
+                }
+
+                pictureBox2.Image = image2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+            else
+            {
+                MessageBox.Show("Please load an image first!");
+            }
+        }
+        private void newImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox pictureBoxOne = new PictureBox();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null && image1 != null)
+            {
+                image2 = new Bitmap(image1.Width, image1.Height);
+                for (int y = 0; y < image1.Height; y++)
+                {
+                    for (int x = 0; x < image1.Width; x++)
+                    {
+                        Color pixelColor = image1.GetPixel(x, y);
+                        int SepiaR = (int)((pixelColor.R * 0.393) + (pixelColor.G * 0.769) + (pixelColor.B * 0.189));
+                        int SepiaG = (int)((pixelColor.R * 0.349) + (pixelColor.G * 0.686) + (pixelColor.B * 0.168));
+                        int SepiaB = (int)((pixelColor.R * 0.272) + (pixelColor.G * 0.534) + (pixelColor.B * 0.131));
+
+                        SepiaR = Math.Min(255, Math.Max(0, SepiaR));
+                        SepiaG = Math.Min(255, Math.Max(0, SepiaG));
+                        SepiaB = Math.Min(255, Math.Max(0, SepiaB));
+
+                        Color newColor = Color.FromArgb(SepiaR, SepiaG, SepiaB);
+                        //the most monkey ass inefficient code ever part 2
+                        image2.SetPixel(x, y, newColor);
+                    }
+                }
+
+
+                pictureBox2.Image = image2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+            else
+            {
+                MessageBox.Show("Please load an image first!");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null && image1 != null)
+            {
+                image2 = new Bitmap(image1.Width, image1.Height);
+                for (int y = 0; y < image1.Height; y++)
+                {
+                    for (int x = 0; x < image1.Width; x++)
+                    {
+                        Color pixelColor = image1.GetPixel(x, y);
+        
+
+                        int grayvalue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                        Color newColor = Color.FromArgb(grayvalue, grayvalue, grayvalue);
+                        //the most monkey ass inefficient code ever
+                        image2.SetPixel(x, y, newColor);
+                    }
+                }
+
+                //find a way to get the pixel color in RGB so i can divide it by 3 and get the gra
+
+                pictureBox2.Image = image2;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            }
+            else
+            {
+                MessageBox.Show("Please load an image first!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.Image = Image.FromFile(fileDialog.FileName);
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+                image1 = new Bitmap(fileDialog.FileName);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           if (pictureBox1.Image != null && image1 != null)
+{
+    image2 = new Bitmap(image1.Width, image1.Height);
+    for (int y = 0; y < image1.Height; y++)
+    {
+        for (int x = 0; x < image1.Width; x++)
+        {
+            Color pixelColor = image1.GetPixel(x, y);
+            int grayvalue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+            Color newColor = Color.FromArgb(grayvalue, grayvalue, grayvalue);
+            image2.SetPixel(x, y, newColor);
+        }
+    }
+
+    // FIX 1: Change from 255 to 256 to include all gray levels (0-255)
+    int[] histogram = new int[256];
+    
+    for(int i = 0; i < histogram.Length; i++)
+    {
+        histogram[i] = 0;
+    }
+
+    // Count each gray level in the image
+    for (int y = 0; y < image2.Height; y++)
+    {
+        for (int x = 0; x < image2.Width; x++)
+        {
+            Color pixelColor = image2.GetPixel(x, y);
+            int grayLevel = pixelColor.R; // In grayscale, R=G=B
+
+            histogram[grayLevel]++;
+        }
+    }
+
+
+    int width = 400;
+    int height = 400;
+    Bitmap histogramGraph = new Bitmap(width, height);
+
+    using (Graphics g = Graphics.FromImage(histogramGraph))
+    {
+        // Fill background
+        g.Clear(Color.White);
+
+        // Find maximum count for scaling
+        int maxCount = 0;
+        for (int i = 0; i < 256; i++)
+        {
+            if (histogram[i] > maxCount)
+                maxCount = histogram[i];
+        }
+
+        // Draw axes
+        Pen axisPen = new Pen(Color.Black, 2);
+        g.DrawLine(axisPen, 40, height - 40, width - 20, height - 40); // X-axis
+        g.DrawLine(axisPen, 40, height - 40, 40, 20); // Y-axis
+
+        // Draw histogram bars
+        Pen barPen = new Pen(Color.Blue, 1);
+        Brush barBrush = new SolidBrush(Color.LightBlue);
+
+        float barWidth = (width - 60) / 256f;
+                        
+        for (int i = 0; i < 256; i++)
+        {
+            if (histogram[i] > 0)
+            {
+                float barHeight = ((float)histogram[i] / maxCount) * (height - 80);
+                float x = 40 + (i * barWidth);
+                float y = height - 40 - barHeight;
+
+                // Draw bar
+                g.FillRectangle(barBrush, x, y, barWidth, barHeight);
+                g.DrawRectangle(barPen, x, y, barWidth, barHeight);
+            }
+        }
+
+        // Draw labels
+        Font labelFont = new Font("Arial", 8);
+        Brush textBrush = new SolidBrush(Color.Black);
+
+        // X-axis labels
+        g.DrawString("0", labelFont, textBrush, 40, height - 30);
+        g.DrawString("128", labelFont, textBrush, 40 + (128 * barWidth), height - 30);
+        g.DrawString("255", labelFont, textBrush, width - 25, height - 30);
+
+        // Axis labels
+        g.DrawString("Pixel Count", labelFont, textBrush, 10, height / 2);
+        g.DrawString("Gray Level", labelFont, textBrush, width / 2 - 30, height - 20);
+
+        // Title
+        Font titleFont = new Font("Arial", 10, FontStyle.Bold);
+        g.DrawString("Grayscale Histogram", titleFont, textBrush, width / 2 - 60, 5);
+
+        // Clean up
+        axisPen.Dispose();
+        barPen.Dispose();
+        barBrush.Dispose();
+        labelFont.Dispose();
+        textBrush.Dispose();
+        titleFont.Dispose();
+    }
+
+    pictureBox2.Image = image2;
+    pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+    // FIX 4: Display histogram in pictureBox3 
+    if (pictureBox3 != null)
+    {
+        pictureBox3.Image = histogramGraph;
+        pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+    }
+}
+else
+{
+    MessageBox.Show("Please load an image first!");
+}
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+          
+
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(fileDialog.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                    
+                image1 = new Bitmap(fileDialog.FileName);
+            }
+
         }
     }
 }
